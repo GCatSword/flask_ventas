73ca4f4106fc7a7cc6cbd82ee4c15f9844e25548
GCatSword <sineg90@hotmail.com>
2020-05-26 18:33:35 +0200

    clase lunes

--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
-nuevo
+Lista de ventas globales
+Instalación
+
+Ejecutar
+
+pip install -r requirements.txt
---
+++ test_flask_app.py
@@ -0,0 +1,15 @@
+import unittest
+from test_base import TestFlaskBase
+
+class TestWeb(TestFlaskBase):
+    def test_server_is_on(self):
+        loquemedevuelve = self.client.get("/")
+        self.assertEqual(loquemedevuelve.status_code, 200)
+
+    def test_route_index_is_lista_regiones(self):
+        loquemedevuelve = self.client.get("/")
+        self.assertEqual(loquemedevuelve.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
